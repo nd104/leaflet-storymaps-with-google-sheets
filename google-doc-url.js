1f183d18be1cc4ee679c448c4931d4c028816ea1
nd104 <62315874+nd104@users.noreply.github.com>
2020-03-17 23:17:52 -0400

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AO6XHL_0JafWZF4KEejkdDNqfuZWUk3SlNlQ6MjlRFM/';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1KVr9gLN08JxVT11ZF38krppDL_6_5T7LZcO_IeVRoYQ/edit#gid=0';
